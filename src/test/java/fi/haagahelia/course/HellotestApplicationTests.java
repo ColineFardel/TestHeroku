e6393b7ea674e5aceffd8bc138abe2ffa314e017
ColineFardel <coline.fardel@gmail.com>
2020-10-14 16:09:53 +0300

    Removing Tests

--- src/test/java/fi/haagahelia/course/HellotestApplicationTests.java
+++ src/test/java/fi/haagahelia/course/HellotestApplicationTests.java
@@ -1,14 +1,5 @@
 package fi.haagahelia.course;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import fi.haagahelia.course.web.HelloController;
 
 /**
  * Testing that the context is creating your controller
@@ -16,15 +7,9 @@ import fi.haagahelia.course.web.HelloController;
  * @author h01270
  *
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
 public class HellotestApplicationTests {
 
-    @Autowired
-    private HelloController controller;
-
-    @Test
     public void contexLoads() throws Exception {
-        assertThat(controller).isNotNull();
     }	
 }
